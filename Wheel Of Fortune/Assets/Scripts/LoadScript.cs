7a8b1cbf1a194599f30725453c9b8386d14a3a31
sandrockman <svc-student@RC37815.student.directory.jccc>
2015-09-16 09:50:48 -0500

    start up at Gather() Function

---
+++ Wheel Of Fortune/Assets/Scripts/LoadScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class LoadScript : MonoBehaviour {
+
+	TextAsset textFile;
+	TextReader reader;
+
+	public List<string> sentences = new List<string>();
+	public List<string> clues = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		textFile = (TextAsset)Resources.Load ("embedded", typeof(TextAsset));
+		reader = new StringReader (textFile.text);
+
+		string lineOfText;
+		int lineNumber = 0;
+
+		//tell teh reader to read a line of text, and store that in the line of TextVariable
+		//continue doing this until there are no lines left
+		while ((lineOfText = reader.ReadLine ()) != null) 
+		{
+			if(lineNumber%2 == 0)
+			{
+				sentences.Add (lineOfText);
+			}
+			else
+			{
+				clues.Add (lineOfText);
+			}
+			lineNumber++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
