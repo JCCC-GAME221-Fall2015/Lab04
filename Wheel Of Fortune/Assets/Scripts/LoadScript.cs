254d43497e760398da1b296d8f86c994932d4aa7
Darrick2theend <svc-student@RC37807.student.directory.jccc>
2015-09-16 09:50:36 -0500

    Added comments
    Comments added to LoadScript script.

--- Wheel Of Fortune/Assets/Scripts/LoadScript.cs
+++ Wheel Of Fortune/Assets/Scripts/LoadScript.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 [RequireComponent(typeof(References))]
 public class LoadScript : MonoBehaviour 
 {
+    // Lists that hold sentences to solve and clues for sentences.
     public List<string> sentences = new List<string>();
     public List<string> clues = new List<string>();
 
@@ -16,11 +17,13 @@ public class LoadScript : MonoBehaviour
     void Start()
     {
         originalFile = new FileInfo(Application.dataPath + "/sentences.txt");
-
+        // Check if a user provided file exists
+        // If file exists, read data from the file
         if (!originalFile.Equals(null) && originalFile.Exists)
         {
             reader = originalFile.OpenText();
         }
+        // If file doesn't exist, read from the embedded game file
         else
         {
             textFile = (TextAsset)Resources.Load("embedded", typeof(TextAsset));
@@ -30,18 +33,22 @@ public class LoadScript : MonoBehaviour
         string lineOfText;
         int lineNumber = 0;
 
+        // Read from the reader file while there is still data in the file
         while((lineOfText = reader.ReadLine()) != null)
         {
+            // Even line number: sentence
             if(lineNumber % 2 == 0)
             {
                 sentences.Add(lineOfText);
             }
+            // odd line number: clue
             else
             {
                 clues.Add(lineOfText);
             }
             lineNumber++;
         }
+        // Call Gather function
         SendMessage("Gather");
     }
 }
