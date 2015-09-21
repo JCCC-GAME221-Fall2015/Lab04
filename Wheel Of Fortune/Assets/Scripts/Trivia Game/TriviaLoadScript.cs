680d6d3ede52661a24cc3cd77d943bd84594ca78
CBroskow <cbroskow@stumail.jccc.edu>
2015-09-20 23:30:36 -0500

    Commit on Sunday evening
    Still need to add Game Over scene

---
+++ Wheel Of Fortune/Assets/Scripts/Trivia Game/TriviaLoadScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(TriviaReferences))]
+public class TriviaLoadScript : MonoBehaviour {
+	
+	FileInfo originalFile;
+	TextAsset textfile;
+	TextReader reader;
+	
+	public List<string> questions = new List<string>();
+	public List<string> answers = new List<string>();
+
+	// Use this for initialization
+	void Start () {
+		
+		originalFile = new FileInfo(Application.dataPath + "/questions.txt");
+		
+		if (originalFile != null && originalFile.Exists)
+		{
+			reader = originalFile.OpenText();
+		}
+		else
+		{
+			textfile = (TextAsset)Resources.Load("embedded2", typeof(TextAsset));
+			reader = new StringReader(textfile.text);
+		}
+		
+		string lineOfText;
+		int lineNumber = 0;
+		
+		while ((lineOfText = reader.ReadLine()) != null)
+		{
+			if (lineNumber%5 == 0)
+			{
+				questions.Add(lineOfText);
+			}
+			else
+			{
+				answers.Add(lineOfText);
+			}
+			
+			lineNumber++;
+		}
+		
+		SendMessage("Gather");
+	} // end method Start
+} // end class TriviaLoadScript
---
+++ Wheel Of Fortune/Assets/Scripts/Wheel of Fortune/LoadScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(References))]
+public class LoadScript : MonoBehaviour {
+
+	FileInfo originalFile;
+	TextAsset textfile;
+	TextReader reader;
+
+	public List<string> sentences = new List<string>();
+	public List<string> clues = new List<string>();
+
+	
+	// Use this for initialization
+	void Start () {
+
+		originalFile = new FileInfo(Application.dataPath + "/sentences.txt");
+
+		if (originalFile != null && originalFile.Exists)
+		{
+			reader = originalFile.OpenText();
+		}
+		else
+		{
+			textfile = (TextAsset)Resources.Load("embedded", typeof(TextAsset));
+			reader = new StringReader(textfile.text);
+		}
+
+		string lineOfText;
+		int lineNumber = 0;
+
+		while ((lineOfText = reader.ReadLine()) != null)
+		{
+			if (lineNumber%2 == 0)
+			{
+				sentences.Add(lineOfText);
+			}
+			else
+			{
+				clues.Add(lineOfText);
+			}
+
+			lineNumber++;
+
+		}
+
+		SendMessage("Gather");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
